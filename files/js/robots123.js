0874dbdf8a76118062a13a0a028af347d7653eb8
LBeckX <lb@1601.com>
2017-09-16 14:42:57 +0200

    Remove line breakes; Change fire rate by pressing and release spacebar

--- files/js/robots123.js
+++ files/js/robots123.js
@@ -1,4 +1,5 @@
 var version = '0.1.5';
+console.log("Game version:",version);
 var is_playing = false;
 var player;
 var hit = 0;
@@ -69,7 +70,6 @@ function load_media() {
 	menu_sprite = new Image();
 	menu_sprite.src = 'files/img/menue.png';
 	
-	/*Sounds*/
 	bgaudio = new Audio('files/media/background-music.MP3');
 	shotaudio = new Audio('files/media/lasergun_cut.mp3');
 	explosionaudio = new Audio('files/media/explosion-flight.mp3');
@@ -81,20 +81,16 @@ function menu() {
 	game_over_menu_buttons = main_menu_buttons;
 
     var menu_buttons="";
-	if(menu_status === "main")
-	{
+	if(menu_status === "main"){
 		menu_buttons = main_menu_buttons;
 	}
-	else if(menu_status === "pause")
-	{
+	else if(menu_status === "pause"){
 		menu_buttons = pause_menu_buttons;
 	}
-	else if(menu_status === "game_over")
-	{
+	else if(menu_status === "game_over"){
 		menu_buttons = game_over_menu_buttons;
 	}
-	if(menu_status === "game_over")
-	{
+	if(menu_status === "game_over"){
 		main_ctx.textBaseline = "middle";
 		main_ctx.textAlign = "center";
 		main_ctx.font = "70px Arial";
@@ -107,19 +103,16 @@ function menu() {
 		main_ctx.fillStyle = "red";
 		main_ctx.fillText("Wave: " + wave + " Score: " + score, 800 / 2, 290);
 	}
-	
-	
+
 	background_ctx.drawImage(menu_sprite,0,0,800,600);
 	
-	for(var i = 0; i < menu_buttons.length; i++)
-	{
+	for(var i = 0; i < menu_buttons.length; i++){
 		var drawX = 600 / 2;
 		var drawY = 350 + i * 60;
 		var width = 200;
 		var height = 50;
 		
-		if(buttons_status[i] === undefined)
-		{
+		if(buttons_status[i] === undefined){
 			buttons_status[i] = "normal";
 			buttons_drawX[i] = drawX;
 			buttons_drawY[i] = drawY;
@@ -127,26 +120,20 @@ function menu() {
 			buttons_height[i] = height;
 		}
 		
-		if(buttons_status[i] === "click")
-		{
-			if(i == 0 && menu_status === "main" || i == 1 && menu_status === "pause" || i === 0 && menu_status === "game_over")
-			{
+		if(buttons_status[i] === "click"){
+			if(i === 0 && menu_status === "main" || i === 1 && menu_status === "pause" || i === 0 && menu_status === "game_over"){
 				new_game();
 			}
-			if(i === 0 && menu_status === "pause")
-			{
+			if(i === 0 && menu_status === "pause"){
 				is_menu = false;
 			}
-			
 			buttons_status[i] = "hover";
 		}
 		
-		if(buttons_status[i] === "hover")
-		{
+		if(buttons_status[i] === "hover"){
 			main_ctx.drawImage(button_press_sprite,drawX,drawY,width,height);
 		}
-		if(buttons_status[i] === "normal")
-		{
+		if(buttons_status[i] === "normal"){
 			main_ctx.drawImage(button_sprite,drawX,drawY,width,height);
 		}
 		
@@ -159,40 +146,31 @@ function menu() {
 		
 		var userNameCookie = Cookie.get('username');
 		
-		if(userNameCookie === null)
-		{
-			if(playerName === '')
-			{
+		if(userNameCookie === null){
+			if(playerName === ''){
 				setCookie();
 			}
 		}
-		else
-		{
+		else{
 			playerName = userNameCookie;
 		}
 	}
 }
 
 function mouse(type,evt) {
-
 	var rect = main_canvas.getBoundingClientRect();
-
     var x = evt.clientX - rect.left;
     var y = evt.clientY - rect.top;
 	
-	for(var i = 0; i < buttons_status.length; i++)
-	{
+	for(var i = 0; i < buttons_status.length; i++){
 		if(x <= buttons_drawX[i] + buttons_width[i] && x >= buttons_drawX[i] && 
-		   y <= buttons_drawY[i] + buttons_height[i] && y >= buttons_drawY[i]
-  		)
-  		{
+		   y <= buttons_drawY[i] + buttons_height[i] && y >= buttons_drawY[i]){
   			if (type === 'move' && buttons_status[i] !== "click")
 				buttons_status[i] = "hover";
 			else
 				buttons_status[i]="click";
 		}
-		else
-		{
+		else{
 			buttons_status[i] = "normal";
 		}
 	}
@@ -212,42 +190,34 @@ function Player() {
 }
 
 Player.prototype.draw = function() {
-	if(this.is_dead === false)
-	{
+	if(this.is_dead === false){
 		this.check_keys();
-		main_ctx.drawImage(main_sprite,this.drawX,this.drawY)
+		main_ctx.drawImage(main_sprite,this.drawX,this.drawY);
 		main_ctx.drawImage(gun_sprite,this.drawX + 60,this.drawY - 20,30,100);
 		
-		if(sendTrue === 1)
-		{
+		if(sendTrue === 1){
 			sendTrue = 0;
 		}
 		
-		if(this.life <= 0)
-		{
+		if(this.life <= 0){
 			this.is_dead = true;
 			this.explode_wait = 50;
 		}
-		if(score > (wave*13) + (hit+20) || this.life > 100)
-		{
+		if(score > (wave*13) + (hit+20) || this.life > 100){
 			score = 0;
 			this.life=0;
 		}
 	}
-	else if(this.explode_wait > 0)
-	{
+	else if(this.explode_wait > 0){
 		main_ctx.drawImage(explosion_sprite,this.drawX,this.drawY,120,120);
 		this.explode_wait--;
 	}
-	else
-	{
+	else{
 		is_menu = true;
 		menu_status = "game_over";
 		
-		if(score >= 0 && score <= 4000)
-			{
-				try
-				{
+		if(score >= 0 && score <= 4000){
+				try{
 					ajaxFunction(playerName,score);
 				}
 				catch(e){alert('Da war ein Fehler')}
@@ -256,33 +226,29 @@ Player.prototype.draw = function() {
 };
 
 Player.prototype.check_keys = function() {
-
-	if(this.is_leftkey === true)
-	{
+	if(this.is_leftkey === true){
 		this.drawX -= this.speed;
-		if(this.drawX <= -20)
-		{
+		if(this.drawX <= -20){
 			this.drawX = -20;
 		}
 	}
-	if(this.is_rightkey === true)
-	{
+	if(this.is_rightkey === true){
 		this.drawX += this.speed;
-		if(this.drawX >= 750)
-		{
+		if(this.drawX >= 750){
 			this.drawX = 750;
 		}
 	}
-	if(this.is_space === true && this.shoot_wait <= 0)
-	{
+	if(this.is_space === true && this.shoot_wait <= 0){
 		bullets[bullets.length] = new Bullet(this.drawX + 68,this.drawY - 30,true);
 		this.shoot_wait = 20;
+	} else if(this.is_space === false && this.shoot_wait > 7){
+        player.shoot_wait = 7;
 	}
-	else
-	{
-		this.shoot_wait--;
+	else{
+		if(this.shoot_wait >=0){
+            this.shoot_wait--;
+		}
 	}
-	
 };
 
 function Enemy() {
@@ -297,8 +263,7 @@ function Enemy() {
 }
 
 Enemy.prototype.draw = function() {
-	if(this.is_dead === false)
-	{
+	if(this.is_dead === false){
 		this.ai();
 		main_ctx.drawImage(fluzeug_sprite,this.drawX,this.drawY);
 		
@@ -309,8 +274,7 @@ Enemy.prototype.draw = function() {
 		main_ctx.fillRect(this.drawX,this.drawY  - 10 + 1,this.life / 30 * 98,8);
 		
 		
-		if(this.life <= 0)
-		{
+		if(this.life <= 0){
 			try {explosionaudio.currentTime = 0;} catch(e){}
 			explosionaudio.volume = soundVol;
 			explosionaudio.play();
@@ -322,8 +286,7 @@ Enemy.prototype.draw = function() {
 			hit += 10;
 		}
 	}
-	else if(this.explode_wait > 0)
-	{
+	else if(this.explode_wait > 0){
 		main_ctx.drawImage(explosion_sprite,this.drawX,this.drawY,120,120);
 		this.explode_wait--;
 	}
@@ -332,29 +295,25 @@ Enemy.prototype.draw = function() {
 Enemy.prototype.ai = function() {
 	this.drawX += this.speed;
 	
-	if(this.drawX >= 800)
-	{
+	if(this.drawX >= 800){
 		this.drawX = -200;
 		this.drawY = 30 + Math.round(Math.random()*170);
 		this.speed = 3 + Math.random() * 5;
 	}
 	
-	if(Math.round(Math.random()*100) === 50)
-	{
+	if(Math.round(Math.random()*100) === 50){
 		bullets[bullets.length] = new Bullet(this.drawX+60,this.drawY + 40);
 	}
 };
 
 function Bullet(x,y,is_Player) {
-	if(is_Player === true)
-	{
+	if(is_Player === true){
 		try {shotaudio.currentTime = 0;} catch(e){}
 		shotaudio.volume = soundVol;
 		shotaudio.play();
 		this.is_Player = true;
 	}
-	else
-	{
+	else{
 		this.is_Player = false;
 	}
 	this.drawX = x;
@@ -367,43 +326,34 @@ function Bullet(x,y,is_Player) {
 }
 
 Bullet.prototype.draw = function() {
-	if(this.explodet === false && this.is_Player === false)
-	{
+	if(this.explodet === false && this.is_Player === false){
 		main_ctx.drawImage(bullet_sprite,this.drawX,this.drawY);
 	}
-	if(this.explodet === false && this.is_Player === true)
-	{
+	if(this.explodet === false && this.is_Player === true){
 		main_ctx.drawImage(laser_sprite,this.drawX,this.drawY);
 	}
 	
-	if(this.explodet === false)
-	{
-		if(this.is_Player === true)
-		{
+	if(this.explodet === false){
+		if(this.is_Player === true){
 			this.drawY -= this.speed;
 		}
-		else
-		{
+		else{
 			this.drawY += this.speed;
 		}
 	}
 	
 	if(this.is_Player === false && this.drawX <= player.drawX + 90 && this.drawX + this.width >= player.drawX
-	&& this.drawY <= player.drawY + 80 && this.drawY + this.height >= player.drawY && this.explodet === false)
-	{
+	&& this.drawY <= player.drawY + 80 && this.drawY + this.height >= player.drawY && this.explodet === false){
 		player.life -= 10;
 		this.explodet = true;
 		this.wait = 50;
 	}
 	
-	if(this.is_Player === true)
-	{
-		for(var i=0; i < enemies.length; i++)
-		{
+	if(this.is_Player === true){
+		for(var i=0; i < enemies.length; i++){
 			if(this.drawX <= enemies[i].drawX + 120 && this.drawX + this.width >= enemies[i].drawX 
 			&& this.drawY <= enemies[i].drawY + 50 && this.drawY + this.height >= enemies[i].drawY && this.explodet === false
-			&& enemies[i].is_dead === false)
-			{
+			&& enemies[i].is_dead === false){
 				this.explodet = true;
 				this.wait = 50;
 				enemies[i].life -= 10;
@@ -413,18 +363,15 @@ Bullet.prototype.draw = function() {
 		}
 	}
 
-	if(this.explodet === true && this.wait > 0)
-	{
+	if(this.explodet === true && this.wait > 0){
 		main_ctx.drawImage(explosion_sprite,this.drawX,this.drawY,50,50);
 		this.wait--;
 	}
 };
 
 function check_wave() {
-	if(spawned_enemies === dead_enemies)
-	{
-		if(is_timeout)
-		{
+	if(spawned_enemies === dead_enemies){
+		if(is_timeout){
 			main_ctx.fillStyle = "black";
 			main_ctx.globalAlpha = 0.7;
 			main_ctx.fillRect(800/2 -300 / 2,600 / 2 - 100 / 2,300,100);
@@ -436,17 +383,14 @@ function check_wave() {
 			main_ctx.font = "50px Arial";
 			main_ctx.fillText("Next Wave!", 800 / 2, 600 / 2);
 		}
-		else
-		{
+		else{
 			is_timeout = true;
-			if(spawned_enemies === 0)
-			{
+			if(spawned_enemies === 0){
 				wave ++;
 				spawn_enemy(wave);
 				is_timeout = false;
 			}
-			else
-			{
+			else{
 				window.setTimeout(function(){wave ++; spawn_enemy(wave);is_timeout = false;},2000);
 			}
 			
@@ -456,8 +400,7 @@ function check_wave() {
 
 function spawn_enemy(n){
 	spawned_enemies += n;
-	for(var i=0;i< n;i++)
-	{
+	for(var i=0;i< n;i++){
 		enemies[i] = new Enemy();
 	}
 }
@@ -465,36 +408,27 @@ function spawn_enemy(n){
 function loop(){
 	main_ctx.clearRect(0,0,800,600);
 	
-	if(is_menu === false)
-	{
-		if(wave <= 3)
-		{
+	if(is_menu === false){
+		if(wave <= 3){
 			background_ctx.drawImage(bg_sprite,0,0);
 		}
-		else if (wave >= 4 && wave <= 7)
-		{
+		else if (wave >= 4 && wave <= 7){
 			background_ctx.drawImage(bg2_sprite,0,0);
 		}
-		else if (wave >= 8 && wave <= 11)
-		{
+		else if (wave >= 8 && wave <= 11){
 			background_ctx.drawImage(bg3_sprite,0,0);
 		}
-		else
-		{
+		else{
 			background_ctx.drawImage(bg4_sprite,0,0);
 		}
-		
-		
-		
+
 		player.draw();
 		
-		for(i=0; i < enemies.length; i++)
-		{
+		for(i=0; i < enemies.length; i++){
 			enemies[i].draw();
 		}
 		
-		for(i=0; i < bullets.length; i++)
-		{
+		for(i=0; i < bullets.length; i++){
 			bullets[i].draw();
 		}
 		
@@ -518,14 +452,12 @@ function loop(){
 		
 		check_wave();
 		}
-	else
-	{
+	else{
 		menu();
 	}
 	
 	
-	if(is_playing)
-	{
+	if(is_playing){
 		requestframe(loop);
 	}
 	
@@ -555,13 +487,11 @@ function stop_loop(){
 
 function key_down(e){
 	var key_id = e.keyCode || e.which;
-	if(key_id === 40 ) // downkey
-	{
+	if(key_id === 40 ){ // downkey
 		player.is_downkey = true;
 		e.preventDefault();
 	}
-	if(key_id === 38 ) // upkey
-	{
+	if(key_id === 38 ){ // upkey
 		player.is_upkey = true;
 		e.preventDefault();
 	}
@@ -570,19 +500,16 @@ function key_down(e){
 		player.is_leftkey = true;
 		e.preventDefault();
 	}
-	if(key_id === 39 ) // rightkey
-	{
+	if(key_id === 39 ){ // rightkey
 		player.is_rightkey = true;
 		e.preventDefault();
 	}
-	if(key_id === 32) // Leertaste
-	{
+	if(key_id === 32){ // Leertaste
 		player.is_space = true;
 		e.preventDefault();
 	}
 	
-	if(key_id === 27 || key_id === 80) // esc or p
-	{
+	if(key_id === 27 || key_id === 80) { // esc or p
 		is_menu = true;
 		menu_status = 'pause';
 	}
@@ -591,67 +518,50 @@ function key_down(e){
 
 function key_up(e){
 	var key_id = e.keyCode || e.which;
-	if(key_id === 40 ) // downkey
-	{
+	if(key_id === 40 ) { // downkey
 		player.is_downkey = false;
 		e.preventDefault();
 	}
-	if(key_id === 38 ) // upkey
-	{
+	if(key_id === 38 ) {  // upkey
 		player.is_upkey = false;
 		e.preventDefault();
 	}
-	if(key_id === 37 ) // leftkey
-	{
+	if(key_id === 37 ){ // leftkey
 		player.is_leftkey = false;
 		e.preventDefault();
 	}
-	if(key_id === 39 ) // rightkey
-	{
+	if(key_id === 39 ){ // rightkey
 		player.is_rightkey = false;
 		e.preventDefault();
 	}
-	if(key_id === 32 ) // spacebar
-	{
+	if(key_id === 32 ){ // spacebar
 		player.is_space = false;
-
-		if(player.shoot_wait <= 0){
-            player.shoot_wait = 5
-		}
-		console.log(player.shoot_wait);
-
 		e.preventDefault();
 	}
 	
 }
 
 function ajaxFunction(playerName20,score10){
-	// Request erzeugen
 	if (window.XMLHttpRequest) {
 		ajaxRequest = new XMLHttpRequest(); // Mozilla, Safari, Opera
-		console.log("Chrome Request");
 	} else if (window.ActiveXObject) {
 		try {
 			ajaxRequest = new ActiveXObject('Msxml2.XMLHTTP'); // IE 5
-            console.log("IE5 Request");
 		} catch (e) {
 			try {
 				ajaxRequest = new ActiveXObject('Microsoft.XMLHTTP'); // IE 6
-                console.log("IE6 Request");
 			} catch (e) {
-				console.log('Error-Request:',e);
+				console.error('ajax error:',e);
 			}
 		}
 	}
 	
 	if(sendTrue !== 1)
 	{
-		if(playerName20 !== playerName || score10 !== score || menu_status !== "game_over")
-		{
+		if(playerName20 !== playerName || score10 !== score || menu_status !== "game_over"){
 			alert("Schwach!");
 		}
-		else
-		{
+		else{
 			var string = "name=" + playerName20 + "&score=" + score10;
 			var url = '/!script/php/score_handler/score_safer.script.php';
 			ajaxRequest.open("post",url,true);
@@ -664,7 +574,6 @@ function ajaxFunction(playerName20,score10){
                         console.log('ajaxErrorCode: ' + ajaxRequest.status);
                 }
             };
-
 			sendTrue = 1;
 		}
 	}
@@ -709,32 +618,28 @@ function setCookie(){
 	var jahrPlus = datum.getFullYear()+1;
 	datum.setFullYear(jahrPlus);
 	playerName = prompt('Bitte geben Sie Ihren Namen ein, um Ihren Score zu speichern.');			
-	if(playerName.length >= 12)
-	{
+	if(playerName.length >= 12){
 		playerName = playerName.substring(0, 12);
 		playerName = playerName.replace(/<\/?[^>]+(>|$)/g, "");
 	}
-	
 	document.cookie="username="+playerName+";domain=unitgreen.com;expires="+datum+";path=/";
 }
 
 	Cookie = {
-			  get: function (name) {
-				var data    = document.cookie.split(";");
-				var cookies = {};
-				for(var i = 0; i < data.length; ++i) {
-				  var tmp = data[i].split("=");
-				  cookies[tmp[0]] = tmp[1];
-				}
-
-				if (name) {
-				  return (cookies[name] || null);
-				} else {
-				  return cookies;
-				}
-			  },
-		 
-		  set: function (name, value) {
-		    document.cookie = name + "=" + value;
-		  }
+		get: function (name) {
+			var data    = document.cookie.split(";");
+			var cookies = {};
+			for(var i = 0; i < data.length; ++i) {
+				var tmp = data[i].split("=");
+				cookies[tmp[0]] = tmp[1];
+			}
+			if (name) {
+				return (cookies[name] || null);
+			} else{
+				return cookies;
+			}
+		},
+		set: function (name, value) {
+			document.cookie = name + "=" + value;
+		}
 };
